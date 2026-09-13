460ad8863f43296b66fddfd6f35fd8933112bd4e
wickidcow <jaywago@gmail.com>
2026-09-13 11:10:25 -0400

    Fix Justifiability registration on modern Slimefun

--- src/main/java/io/taraxacum/finaltech/core/item/unusable/Justifiability.java
+++ src/main/java/io/taraxacum/finaltech/core/item/unusable/Justifiability.java
@@ -1,33 +1,21 @@
 package io.taraxacum.finaltech.core.item.unusable;
 
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
-import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
-import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
-import io.taraxacum.finaltech.FinalTechChanged;
 import io.taraxacum.finaltech.FinalTechChanged;
 import io.taraxacum.finaltech.core.interfaces.RecipeItem;
-import io.taraxacum.finaltech.core.item.machine.range.point.EquivalentConcept;
 import io.taraxacum.finaltech.util.RecipeUtil;
 import io.taraxacum.libs.plugin.dto.ItemWrapper;
 import io.taraxacum.libs.plugin.util.ItemStackUtil;
 import io.taraxacum.libs.slimefun.interfaces.SimpleValidItem;
 import io.taraxacum.libs.slimefun.util.SfItemUtil;
-import me.mrCookieSlime.CSCoreLibPlugin.Configuration.Config;
-import me.mrCookieSlime.Slimefun.Objects.handlers.BlockTicker;
-import me.mrCookieSlime.Slimefun.api.BlockStorage;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Random;
- 
+
 public class Justifiability extends UnusableSlimefunItem implements RecipeItem, SimpleValidItem {
     private final ItemWrapper templateValidItem;
 
@@ -36,35 +24,6 @@ public class Justifiability extends UnusableSlimefunItem implements RecipeItem, 
         ItemStack validItem = new ItemStack(this.getItem());
         SfItemUtil.setSpecialItemKey(validItem);
         this.templateValidItem = new ItemWrapper(validItem);
-    }
-    @Override
-    public void preRegister() {
-        this.addItemHandler(new BlockTicker() {
-            public void tick(Block b, SlimefunItem sf, Config data) {
-                try {
-                    if (FinalTechChanged.y) {
-                        FinalTechChanged.getInstance().getServer().getScheduler().runTask(FinalTechChanged.getInstance(), () -> b.setType(Material.AIR));
-                        BlockStorage.clearBlockInfo(b.getLocation());
-                        return ;
-                    }
-                    if (BlockStorage.getLocationInfo(b.getLocation(), "life") != null) {
-                        int i = Integer.parseInt(BlockStorage.getLocationInfo(b.getLocation(), "life"));
-                        if (i == 0) {
-                            FinalTechChanged.getInstance().getServer().getScheduler().runTask(FinalTechChanged.getInstance(), () -> b.setType(Material.AIR));
-                            BlockStorage.clearBlockInfo(b.getLocation());
-                            return ;
-                        }
-                        i--;
-                        BlockStorage.addBlockInfo(b.getLocation(), "life", String.valueOf(i));
-                    } else BlockStorage.addBlockInfo(b.getLocation(), "life", "5");
-                } catch (Exception ignore) {
-                }
-            }
-
-            public boolean isSynchronized() {
-                return false;
-            }
-        });
     }
 
     @Nonnull
